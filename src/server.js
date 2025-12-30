71bf5f4ff1f5bb64234128040d82b86a291ae934
Le, Thi Linh Chi <linhchi1152004@gmail.com>
2025-12-30 23:10:44 +0100

    File for environment variables created

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 3000; //process.env ist ein Obj, das alle Umgebungsvariablen enthält
 
-app.listen(30000, () => {
-    console.log('Server is running on http://localhost:30000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
