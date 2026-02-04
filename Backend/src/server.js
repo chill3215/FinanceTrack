86af88f2ecff44fb75a3b7658d418fbc439547d6
Thi Linh Chi Le <163923575+chill3215@users.noreply.github.com>
2026-02-04 21:50:28 +0100

    Rate limiting middleware added
    Added rate limiting to the Express server to limit requests.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import cors from "cors";
 //importiert damit die Verbindung initialisiert wird, danach benutze nur models
 import db from "./db";
+import rateLimit from "express-rate-limit";
 import plaidRoutes from "./routes/plaid.routes.js";
 import authRoutes from "./routes/auth.routes";
 import passport from "passport";
@@ -12,6 +13,16 @@ import passport from "passport";
 const app = express();
 const PORT = process.env.PORT; // process.env ist ein Obj, das alle Umgebungsvariablen enthält
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    // store: ... , // Redis, Memcached, etc. See below.
+})
+
+app.use(limiter);
 app.use(cors());
 
 // Test route
